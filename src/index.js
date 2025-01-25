401f792942fcec740d129a76c54465ef4243b676
Kostiantyn <kostya20022210@gmail.com>
2025-01-25 20:20:41 +0200

    Refactor Country class to function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-class Country {
-    constructor(title, capital, population, area) {
-        this.title = title;
-        this.capital = capital;
-        this.population = Number(population);
-        this.area = Number(area);        
-    }
+function Country(title, capital, population, area) {   
+    this.title = title;
+    this.capital = capital;
+    this.population = population;
+    this.area = area;
+}
 
-    showCountry() {
-        console.log(`title: ${this.title}, capital: ${this.capital}, population: ${this.population}, area: ${this.area}`)        
+/**
+ * Logs all info about the country
+ */
+Country.prototype.showCountry = function () {
+    console.log(`title: ${this.title}, capital: ${this.capital}, population: ${this.population}, area: ${this.area}`);
+}
+
+/**
+ * Logs all fields of the object except functions
+ * @param {object} obj 
+ */
+function showObjectFields(obj) {
+    for (const key in obj) {
+        if (typeof obj[key] != 'function') {
+            console.log(`obj.${key} = ${obj[key]}`);
+        }
     }
 }
 
-const country = new Country('tit', 'tut', 123, 321);
-country.showCountry();
+const ukraine = new Country('Ukraine', 'Kyiv', 41_000_000, 603_500);
+const poland = new Country('Poland', 'Warsaw', 38_000_000, 312_696);
+
+console.log('======================');
+ukraine.showCountry();
+showObjectFields(ukraine);
+console.log('======================');
+poland.showCountry();
+showObjectFields(poland);
